db5a0730e8f2f6825f8e6900d795cf329834fdc1
Auri A. Rahimzadeh <auri@auri.net>
2017-11-26 10:17:38 -0500

    Created mobile app. Still need to wire up correct API URL.

--- ElevenNoteMobileApp/ElevenNoteMobileApp/App.xaml.cs
+++ ElevenNoteMobileApp/ElevenNoteMobileApp/App.xaml.cs
@@ -1,34 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿using ElevenNoteMobileApp.Contracts;
+using ElevenNoteMobileApp.Services;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
+// Define dependencies for injection.
+[assembly: Xamarin.Forms.Dependency(typeof(FakeNoteService))]
 namespace ElevenNoteMobileApp
 {
-	public partial class App : Application
-	{
-		public App ()
-		{
-			InitializeComponent();
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class App : Application
+    {
+        /// <summary>
+        ///  Note service access. We set our dependencies above the namespace declaration.
+        /// </summary>
+        internal static readonly INoteService NoteService = DependencyService.Get<INoteService>();
 
-			MainPage = new ElevenNoteMobileApp.MainPage();
-		}
+        public App()
+        {
+            InitializeComponent();
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+            MainPage = new ElevenNoteMobileApp.MainPage();
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
