3ede363e18b202132bc893eed53cf355f9f897d5
Auri A. Rahimzadeh <auri@auri.net>
2017-11-30 07:23:55 -0500

    End of Xamarin Day 1 - Created Base App, Added Dependency

---
+++ ElevenNoteMobileApp/ElevenNoteMobileApp/Services/FakeNoteService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ElevenNote.Models;
+using ElevenNoteMobileApp.Contracts;
+
+namespace ElevenNoteMobileApp.Services
+{
+    public class FakeNoteService : INoteService
+    {
+        public Task<bool> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<NoteListItem>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NoteDetail> GetById(int noteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AddNew(NoteCreate note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(NoteEdit note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int noteId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
